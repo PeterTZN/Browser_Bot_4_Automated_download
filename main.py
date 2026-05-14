187119919751bedf47804fb1ea7c2fbf2b22048a
PeterTZN <jeep11@hotmail.com>
2026-05-14 12:35:41 +0100

    Define driver, options, and service Load the webpage Locate username, password and login button

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
+# Define driver, options, and service
+chrome_options = Options()
+chrome_options.add_argument("--disable-search-engine-choice-screen")
 service = Service('chromedriver-mac-arm64/chromedriver')
-driver = webdriver.Chrome(service=service)
+driver = webdriver.Chrome(options=chrome_options, service=service)
+
+# Load the webpage
 driver.get('https://demoqa.com/login')
+
+# Locate username, password and login button
+username_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'userName')))
+password_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'password')))
+
+username_field.send_keys('Python_learning')
+password_field.send_keys('@Python1977')
 
 input("Press Enter to close the browser")
 driver.quit()
